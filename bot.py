9bbe31dbdd961ba7d1e207fe54d6985540d47d4d
Blockman02135 <65733896+Blockman02135@users.noreply.github.com>
2020-05-22 09:19:53 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -49,6 +49,62 @@ async def settings(ctx, type, type2, display):
 			usedcmd(f'{ctx.message.author}','settings setdisplay activity')
 			print(f'[INFO]Bot changed activity display text to: {display}')
 
+
+async def clear(ctx, amount):
+    await ctx.channel.purge(limit=int(amount)+1)
+    await ctx.channel.send(embed=discord.Embed(description=f"{ctx.author.mention} отчистил чат на {amount} сообщений", color = 0xFF9900))
+    print(f":: {ctx.author} отчистил канал {ctx.channel} на {amount} сообщений")
+
+
+
+@Bot.command()
+async def serverinfo(ctx):
+    members = ctx.guild.members
+    online = len(list(filter(lambda x: x.status == discord.Status.online, members)))
+    offline = len(list(filter(lambda x: x.status == discord.Status.offline, members)))
+    idle = len(list(filter(lambda x: x.status == discord.Status.idle, members)))
+    dnd = len(list(filter(lambda x: x.status == discord.Status.dnd, members)))
+    allchannels = len(ctx.guild.channels)
+    allvoice = len(ctx.guild.voice_channels)
+    alltext = len(ctx.guild.text_channels)
+    allroles = len(ctx.guild.roles)
+    embed = discord.Embed(title=f"{ctx.guild.name}", color=0xff0000, timestamp=ctx.message.created_at)
+    embed.description=(
+        f":timer: Сервер создали **{ctx.guild.created_at.strftime('%A, %b %#d %Y')}**\n\n"
+        f":flag_white: Регион **{ctx.guild.region}\n\nГлава сервера **{ctx.guild.owner}**\n\n"
+        f":tools: Ботов на сервере: **{len([m for m in members if m.bot])}**\n\n"
+        f":green_circle: Онлайн: **{online}**\n\n"
+        f":black_circle: Оффлайн: **{offline}**\n\n"
+        f":yellow_circle: Отошли: **{idle}**\n\n"
+        f":red_circle: Не трогать: **{dnd}**\n\n"
+        f":shield: Уровень верификации: **{ctx.guild.verification_level}**\n\n"
+        f":musical_keyboard: Всего каналов: **{allchannels}**\n\n"
+        f":loud_sound: Голосовых каналов: **{allvoice}**\n\n"
+        f":keyboard: Текстовых каналов: **{alltext}**\n\n"
+        f":briefcase: Всего ролей: **{allroles}**\n\n"
+        f":slight_smile: Людей на сервере **{ctx.guild.member_count}\n\n"
+    )
+
+    embed.set_thumbnail(url=ctx.guild.icon_url)
+    embed.set_footer(text=f"ID: {ctx.guild.id}")
+    embed.set_footer(text=f"ID Пользователя: {ctx.author.id}")
+    await ctx.send(embed=embed)
+
+
+
+#Calculator===================
+OPERATIONS = {
+    '+': lambda x, y: x + y,
+    '-': lambda x, y: x - y,
+    '*': lambda x, y: x * y,
+    '/': lambda x, y: x / y
+}
+
+@Bot.command()
+async def calc(ctx, a, operator, b):
+    await ctx.send(f"{a} {operator} {b} = " + str(OPERATIONS[operator](int(a), int(b))))
+
+
 #@Bot.command()
 #async def help(ctx, page):
 #	if page == '1':
